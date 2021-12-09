edd14717f0a3051411e096fb0f537eb25724b24b
Shahad-F <shahadbinsweleh@gmail.com>
2021-12-09 18:33:00 +0300

    style 6

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,23 @@
   
 import './App.css';
 import Nav from './Nav'
+ 
+
+ 
+ 
 
 function App() {
   return (
     <>
+    <div className="header">
+       
+    
     <Nav />
     
+    </div>
+    <div className='footer'>
+
+    </div>
     </>
   );
 }
