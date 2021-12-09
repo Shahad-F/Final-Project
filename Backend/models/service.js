2b0ce3dc2c847955f19972ba3f6a0451b28579e6
Shahad-F <shahadbinsweleh@gmail.com>
2021-12-09 10:04:29 +0300

    schema done

--- Backend/models/service.js
+++ Backend/models/service.js
@@ -4,7 +4,20 @@ const Schema =mongoose.Schema
 
 const ServiceSchema = new Schema({
 
+nameOfService:{
+    type:String,
+    required:[true,"Name of service is required ."],
+    unique:true
+},
+image:{
+    type:String,
+    required:[true,"Image is required"],
+    unique:true
+}
 
-
+// object of service.
     
 })
+
+
+module.exports =mongoose.model('Service',nameOfService)
