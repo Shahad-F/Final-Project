ec079ee115b07c7187a3f2eb420b73a4878d936d
Shahad-F <shahadbinsweleh@gmail.com>
2021-12-09 21:03:29 +0300

    style 9

--- frontend/src/components/About.js
+++ frontend/src/components/About.js
@@ -3,6 +3,12 @@ import '../CSS/Home.css'
     return (  <>
      <h1>About us</h1>
     
+    <h2>  this website is for women who own a car,
+         and sometimes they have a problem when she driving the car,
+          they can't fix it like the fuel runs out, 
+          the wheels are damaged, needs to move her car, 
+          needs an instructor to teach her how to drive.
+So this website will help her.</h2>
     </>
     );
 }
