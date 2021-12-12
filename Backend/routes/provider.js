48b6abba3c492987b0466f12c7250dc3257eb1cd
Shahad-F <shahadbinsweleh@gmail.com>
2021-12-12 08:41:02 +0300

    login for provider in frontend

--- Backend/routes/provider.js
+++ Backend/routes/provider.js
@@ -4,9 +4,9 @@ ProviderController = require("../controllers/provider")
 
 
 router.get('/',ProviderController.index)
-router.post('/provider/:uid',ProviderController.create)
-router.put('/provider/:uid',ProviderController.update)
-router.delete('/provider/:uid',ProviderController.delete)
+// router.post('/c/:uid',ProviderController.create)
+router.put('/update/:uid',ProviderController.update)
+router.delete('/delete/:uid',ProviderController.delete)
 router.post('/signUp',ProviderController.signUp)
 router.post("/login",ProviderController.authenticate)
 
--- frontend/src/components/Provider/SignupProvider.js
+++ frontend/src/components/Provider/SignupProvider.js
@@ -1,11 +1,19 @@
 
 import { Link} from 'react-router-dom'
-
+import {useState,useEffect} from 'react'
+import axios from 'axios'
+import swal from 'sweetalert';
 
 function SignUp() {
     return ( <>
+    <form className='Psignup'> 
+
     <h2>SignUp page</h2>
+
+
 <p>I have account <Link to={`/ProviderLogin`}>Login</Link></p>
+    </form>
+     
 
     </> );
 }
