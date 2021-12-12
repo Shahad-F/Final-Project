4d5ccc5a03c2707d8c8360389ee419fd46064edc
Shahad-F <shahadbinsweleh@gmail.com>
2021-12-12 21:40:05 +0300

    bootstrap 7

--- frontend/src/logo.js
+++ frontend/src/logo.js
@@ -2,7 +2,8 @@
  import './CSS/Home.css'
  //import logo from  './img/Picture1.png'
  //import logo from './img/Picture3.png'
- import logo from './img/Picture4.png'
+//  import logo from './img/Picture4.png'
+ import logo from './img/Picture2.png'
  function Logo() {
      return (  <div className='logo-main'>
      <img src= {logo} alt='' width={100}/>
