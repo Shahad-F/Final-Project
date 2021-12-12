37777ef61251f0eb04fb0355577f68811931bc77
Shahad-F <shahadbinsweleh@gmail.com>
2021-12-12 08:51:40 +0300

    login for provider in frontend 3

--- frontend/src/components/Provider/SignupProvider.js
+++ frontend/src/components/Provider/SignupProvider.js
@@ -14,22 +14,45 @@ function SignUp() {
  const [Phone ,setPhone]=useState();
  const [Image ,setImage]=useState();
  const [City ,setCity]=useState();
-  
 const [Email,setEmail]=useState()
 const [Password,setPassword]=useState()
 
-// firstName:req.body.firstName,
-// lastName:req.body.lastName,
-// phone:req.body.phone,
-// image:req.body.image,
-// city:req.body.city,
-// price:req.body.price
+ 
 
     return ( <>
     <form className='Psignup'> 
 
     <h2>SignUp page</h2>
 
+<input type='text' name='first name'
+placeholder='First Name' 
+onChange={e=>setFName(e.target.value)}/>
+
+<input type='text' name='last name'
+placeholder='Last Name' 
+onChange={e=>setLName(e.target.value)} />
+
+<input type='text' name='phone'
+placeholder='Phone Number' 
+onChange={e=>setPhone(e.target.value)} />
+
+<input type='text' name='image'
+placeholder='put your image' 
+onChange={e=>setImage(e.target.value)}/>
+
+<input type='text' name='city'
+placeholder='your city' 
+onChange={e=>setCity(e.target.value)}/>
+
+<input type='text' name='email'
+placeholder='your Email' 
+onChange={e=>setEmail(e.target.value)}/>
+
+<input type='password' name='password'
+placeholder='password' 
+onChange={e=>setPassword(e.target.value)}/>
+
+<button onClick={(e)=>SignUp(e)}>Sign Up</button>
 
 <p>I have account <Link to={`/ProviderLogin`}>Login</Link></p>
     </form>
