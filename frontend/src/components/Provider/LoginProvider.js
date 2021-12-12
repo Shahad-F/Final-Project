837f59bdcc956d4e6c0486963e1b010b117ec63a
Shahad-F <shahadbinsweleh@gmail.com>
2021-12-12 10:09:08 +0300

    login for provider 2

--- frontend/src/components/Provider/LoginProvider.js
+++ frontend/src/components/Provider/LoginProvider.js
@@ -1,9 +1,73 @@
-
+import {Link} from 'react-router-dom'
+import {useState} from 'react'
+import{useNavigate} from 'react-router-dom'
+import jwt from 'jwt-decode'
+import axios from 'axios'
+import swal from 'sweetalert'
 
 
 function Login() {
+    const navigate=useNavigate()
+    const [Email ,setEmail] =useState()
+    const [Password ,setPassword] =useState()
+
+
+const handelLogin=(e)=>{
+
+    e.preventDefault()
+
+    axios.post('http://localhost:3030/providers/login', {
+        email:Email,password:Password})
+
+        .then((res)=>{
+            console.log(res.data)
+
+            if(res.data.success === true){
+                const token = res.data.token;
+                const providersign = jwt(token);
+                console.log(token)
+                console.log(providersign)
+                localStorage.setItem('token',token);
+
+                swal({
+                    title:'Welcome .'+Email,
+                    icon:'success'
+                  })
+                    navigate('/ServiceHome');
+            }
+            else{
+                swal({
+                    title: "Your password or email is incorrect",
+                   
+                    icon:'error', 
+                     button: "Try again "
+                  })  
+            }
+        })
+}
+
     return ( <>
-    <h2>Login page</h2>
+  <form className="loginForm">
+        
+        <h1>Login</h1>
+    
+            <input type='email'
+            placeholder='Enter your email address.'
+            onChange={e=>setEmail(e.target.value)}
+            />
+    <br/>
+    
+    <input type='password'
+            placeholder='Enter your password.'
+            onChange={e=>setPassword(e.target.value)}
+            />
+    <br/>
+    
+    <button className='LOGIN'
+     onClick={(e)=>handelLogin(e)}> Login</button>
+     
+        </form>
+         
     </> );
 }
 
--- frontend/src/components/Provider/ServiceHome.js
+++ frontend/src/components/Provider/ServiceHome.js
@@ -1,7 +1,47 @@
+import { useNavigate } from "react-router-dom"
+import {useState,useEffect} from 'react'
+import axios from 'axios'
+import swal from 'sweetalert';
+import '../admin/service.css'
+
+
 function ServiceHome() {
+    const navigate=useNavigate()
+    const [service,setService]=useState([]);
+    const [newService,setNewService]=useState({});
+
+
+    // display all Services
+    useEffect(()=>{ 
+
+        axios.get('http://localhost:3030/services')
+        .then((res)=>{
+            console.log(res.data)
+            setService(res.data)
+             
+        })
+         },[newService])
     return ( <>
     
     <h2>Service Home</h2>
+
+    <div className="bigBox">
+    {service.map((item ,index)=>{
+        return <div key={index} className="box">
+       
+       <img src={item.image} alt='' width={200}/>
+       <div className="title">{item.nameOfService}</div>
+      
+      {/* <div> 
+        <button className="editbtn" onClick={()=>handelEditService(item._id)}>Updae</button>
+       <button className='deletebtn' onClick={()=>handelDeleteService(item._id)}> Delete</button>
+       </div> */}
+
+        </div>
+    })}
+</div>
+
+
     </> );
 }
 
