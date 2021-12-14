99ef4d5263d3646e4932de6160152bb48cdaf90a
Shahad-F <shahadbinsweleh@gmail.com>
2021-12-14 08:05:17 +0300

    stylre

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -5,7 +5,7 @@
 
  import 'bootstrap/dist/css/bootstrap.min.css';
  
- import { Form ,Col,Image,Card} from 'react-bootstrap';
+ import { Container ,Row,Image,Button,Col} from 'react-bootstrap';
  
 
 
@@ -25,9 +25,10 @@ const handelProvider=()=>{
     navigate('/ProviderSignUp')
 }
     return (  <>
+
+
     <div className="parent"> 
 
-    
          <div className="subject"> 
 
         <h1>CALL <span>me</span></h1>
@@ -39,20 +40,17 @@ const handelProvider=()=>{
 
 </div>
         </div>
-
-        {/* <div className='carImage'> */}
-        {/* <img src='https://www.pngkit.com/png/full/100-1008399_luxury-car-png-giugiaro-berlina.png' alt='' width={500}/> */}
-        {/* <img src='https://www.pngkit.com/png/full/71-716885_free-png-cadillac-png-images-transparent-swift-car.png' alt='' width={530}/> */}
-        {/* <img src='https://www.pngkit.com/png/full/956-9566853_las-vegas-exotic-car-rental-2015-ferrari-458.png' alt='' width={530}/> */}
+        
+        
         <Container>
           <Row>
+          <Col xs={6} md={4}>
         <Image src='https://www.pngkit.com/png/full/89-892591_grey-mercedes-benz-e-class-car-png-image.png' alt='' width={500}/>
+        </Col>
         </Row>
-</Container>
-         {/* <img src='https://www.pngkit.com/png/full/970-9708324_antique-classic-car-green-vintage-mosaic-clipart-car.png' alt='' width={500}/> */}
-         {/* <img src='https://www.pngkit.com/png/full/79-795919_old-car-style-cars-public-domain-vintage-car.png' alt='' width={500}/> */}
- 
-     {/* </div> */}
+       </Container>
+         
+      
     
         
         </div>
