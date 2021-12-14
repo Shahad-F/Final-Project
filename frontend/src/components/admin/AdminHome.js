dc71c54912dde877ff4ac4ac627cf9a8d1780353
Shahad-F <shahadbinsweleh@gmail.com>
2021-12-14 14:07:49 +0300

    admin home page 3

--- frontend/src/components/admin/AdminHome.js
+++ frontend/src/components/admin/AdminHome.js
@@ -3,7 +3,7 @@ import './admin.css'
 import{useNavigate} from 'react-router-dom'
 import 'bootstrap/dist/css/bootstrap.min.css';
  
- import { Container ,Row,Image,Button,Col} from 'react-bootstrap';
+ import { Card,Button,Col} from 'react-bootstrap';
  
 
 
@@ -30,22 +30,47 @@ navigate('/AddNewAdmin')
     onClick={handelBack}
     >Back to home page</Button>{' '}
     
-<div className="Parent"> 
+{/*  */}
+
+<Card className="Parent"> 
+
+<Card style={{ width: '20rem' }}>
+  <Card.Img variant="top" src="https://i.pinimg.com/originals/07/cf/df/07cfdf825b9b50d3413309a14c9e4ed6.jpg" />
+  <Card.Body>
+    <Card.Title> Add New Admin</Card.Title>
+     
+    <Button variant="primary" onClick={handelAddNewAdmin}>Click here</Button>
+  </Card.Body>
+</Card>
+<Card style={{ width: '20rem' }}>
+  <Card.Img variant="top" src="https://a-static.besthdwallpaper.com/black-audi-r8-vehicle-on-road-between-trees-during-daytime-wallpaper-1280x1024-26154_32.jpg" />
+  <Card.Body>
+    <Card.Title> Add New Service</Card.Title>
+     
+    <Button variant="primary" onClick={handelAddNewService}>Click here</Button>
+  </Card.Body>
+</Card>
+</Card>
+
+{/*  */}
+
+
+{/* <div className="Parent"> 
 
     <div className='child-1'>
 
         <button className='btn' onClick={handelAddNewAdmin} >Add new Admin</button> <br></br>
         <button className='btn' onClick={handelAddNewService}>Add new Service</button>
-    </div>
+    </div> */}
 
 
-<div>
+{/* <div>
 
     <img src='https://www.pngkit.com/png/full/1-19431_pngpix-com-blue-ferrari-california-t-car-png.png' alt ='' width={550}/>
-</div>
+</div> */}
     
 
-</div> 
+ 
      </> );
 }
 
