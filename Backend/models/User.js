4c9aed3b69a0bbb3fa91d843ef46732e3fc36e5b
Shahad-F <shahadbinsweleh@gmail.com>
2021-12-15 10:50:17 +0300

    create model of user

---
+++ Backend/models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema
+const passportLocalMongoose = require('passport-local-mongoose');
+
+
+const UserSchema = new Schema({
+
+
+    firstName:{
+        type:String,
+        required:[true,'firstName should be provided']
+    },
+    lastName:{
+        type:String,
+        required:[true,'lasttName should be provided']
+    },
+    phone:{
+        type:Number,
+        required:[true,'phone should be provided']
+    },
+    email:{
+        type:String, 
+        required:[true,'Admin email should be provided'],
+        unique:true
+    },
+    image:{
+        type:String,
+        required:[true,'image should be provided']},
+    city:{
+        type:String,
+        required:[true,'city should be provided'],
+    },
+
+})
+
+UserSchema.plugin(passportLocalMongoose,{
+    usernameField:'email'
+})
