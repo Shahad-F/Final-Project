037f744e09f3ed3431ef0d07ceca6b5e14c939ae
Shahad-F <shahadbinsweleh@gmail.com>
2021-12-16 08:25:09 +0300

    homepage for user 2

--- frontend/src/components/User/UserHome.js
+++ frontend/src/components/User/UserHome.js
@@ -1,8 +1,51 @@
+
+import { useNavigate } from "react-router-dom"
+import {useState,useEffect} from 'react'
+import axios from 'axios'
+import swal from 'sweetalert';
+import {Link} from 'react-router-dom'
+import '../admin/service.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+ import Button from 'react-bootstrap/Button';
+ import { Form ,Col,Image,Card} from 'react-bootstrap';
+
 function UserHome() {
+    const navigate=useNavigate()
+    const [service,setService]=useState([]);
+    const [newService,setNewService]=useState({});
+
+    // display all Services
+    useEffect(()=>{ 
+
+        axios.get('http://localhost:3030/services')
+        .then((res)=>{
+            console.log(res.data)
+            setService(res.data)    
+        })
+         },[newService])
+
+        // 
     return (  <>
     
-    <h1>User Home </h1>
+    <h1 className="lineTitle">User Home </h1>
     
+    <Card className="BigCard">
+    {service.map((item ,index)=>{
+        return <Card key={index} className="box">
+       
+       <Image src={item.image} alt='' width={200} roundedCircle />
+       <Card.Body className="title">{item.nameOfService} </Card.Body>
+      
+      <br></br>
+       <Button variant="link" className='link'>
+        <Link to={`/AddProviderService/${item._id}`}>See More</Link></Button>{' '}
+        
+        </Card>
+    })}
+</Card>
+
+
     </>);
 }
 
