fc82db34b2aa272981442c9c3c5f895b53f4a6b7
Shahad-F <shahadbinsweleh@gmail.com>
2021-12-16 08:36:12 +0300

    homepage for user 4

--- frontend/src/components/User/AllProvider.js
+++ frontend/src/components/User/AllProvider.js
@@ -1,4 +1,22 @@
+
+import React,{useEffect,useState} from 'react'
+import {useParams,useNavigate} from "react-router-dom"
+import axios from 'axios'
+
+import '../Provider/sign.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css'
+import {Card} from "react-bootstrap"
+
 function AllProvider() {
+
+    const {_id}=useParams();
+const[service,setService]=useState([])
+const[newService,setNewService]=useState({})
+const [loading,setLoading]=useState(true)
+
+
+
     return ( <>
     <h1>All Provider</h1>
     </> );
