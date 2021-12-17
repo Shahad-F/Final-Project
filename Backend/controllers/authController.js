f37f24e2ed1c4ea36194b0e19f4be3e5a5784aed
Shahad-F <shahadbinsweleh@gmail.com>
2021-12-17 20:43:33 +0300

    jwt 8

--- Backend/controllers/authController.js
+++ Backend/controllers/authController.js
@@ -94,10 +94,13 @@ const {email ,password} =req.body;
 
 
  try{
-const user = await User.login(email,password)
-res.status(200).json({user:user._id})
+    const user = await User.login(email,password);
+    const token = createToken(user._id);
+    res.cookie('jwt', token ,{httpOnly:true, maxAge: maxAge * 1000});
+    res.status(200).json({user: user._id});
  }
  catch(err){
-     res.status(400).json({ })
+     const errors = handelErrors(err);
+     res.status(400).json({errors})
  }
 }
